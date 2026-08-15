2ae9ffe305d27804aa9f8ae867dc16b05738a26a
Shayan-Naeem <shayan.naeem007@gmail.com>
2026-08-15 20:18:19 +0500

    Added school.py & tested demo.py

---
+++ school.py
@@ -0,0 +1,16 @@
+#no_of_months = 6
+#per_month_fee = 20000
+#total_fee = per_month_fee * no_of_months
+#print(f"Your Net Fee will be {total_fee}")
+
+english = 80
+french = 75
+maths = 70
+science = 60
+it = 80
+
+total_marks = 100*5
+obtained_marks = english + french + maths + science + it
+percentage = obtained_marks / total_marks * 100
+
+print(f"Your Obtained Marks are: {obtained_marks}\n You got {percentage}% ")
